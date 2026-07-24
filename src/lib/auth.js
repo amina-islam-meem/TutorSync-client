9fcf2bb753306d373fd87695651c55fced420cd9
amina-islam-meem <amina.meem28@gmail.com>
2026-07-24 17:40:41 +0600

    fix betterauth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -9,6 +9,8 @@ await client.connect();
 const db = client.db("tutorsync");
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,   
+  secret: process.env.BETTER_AUTH_SECRET, 
   database: mongodbAdapter(db),
 
   emailAndPassword: {
